43b8f1683c697e18579f4886a0228ced367aa5d8
ritesh kumar <riteshrkj21@gmail.com>
2026-02-18 22:00:25 +0530

    removed credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://riteshkumar:riteshrkj456@cluster0.wxs7z.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+    await mongoose.connect('**************************************').then(()=>console.log("DB Connected"))
 }
 
 
 // add your mongoDB connection string above.
 // Do not use '@' symbol in your databse user's password else it will show an error.
+
